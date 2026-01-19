fa01ccf4b02bb0a509ac2675f1f3cb6f522141fa
diy_code <saiopencv@gmail.com>
2026-01-20 00:08:33 +0530

    first commit

---
+++ full_adder/main.py
@@ -0,0 +1,2 @@
+import shrike
+shrike.flash("full_adder.bin")  #use the bit stream file name created in this project
---
+++ led_blink/main.py
@@ -0,0 +1,2 @@
+import shrike
+shrike.flash("led_blink.bin")  #use the bit stream file name created in this project
